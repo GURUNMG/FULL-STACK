d398211f91dd310e8c7a4e2d36c5c63c29379e29
GURUBHARAN N M <gurubharan.cs20@bitsathy.ac.in>
2023-02-18 14:44:07 +0530

    expenses json

--- EXPENSE TRACKER/app.js
+++ EXPENSE TRACKER/app.js
@@ -7,6 +7,7 @@ const port=3000
 //   res.send("Testing successfull status:200")
 // })
 
+// api used to testing whether the server is running
 app.get('/api/v1/health',(req, res)=>{
   // res.status(201) then 201 is displayed in the postman
   res.status(201).json({
@@ -14,6 +15,19 @@ app.get('/api/v1/health',(req, res)=>{
     status:"Successs"
   })
 })
+
+// list of expenses done by user
+const expenses=[
+  {id:1, name:"MOVIE", amount:200, desc:"VAATHI"},
+  {id:2, name:"FOOD", amount:500, desc:"BDAY TREAT"},
+  {id:3, name:"COMPETITION", amount:300, desc:"INTER CLG"}
+]
+
+// to get all the expenses from the server by the user
+app.get("/api/v1/expenses", (req, res)=>{
+  res.send(expenses)
+})
+
 app.listen(port, ()=>{
   console.log("Server is running on port number",port);
 })
