429af9f77641ddc7491b0c9fb7c70ffadecaad7c
GURUBHARAN N M <gurubharan.cs20@bitsathy.ac.in>
2023-02-25 15:13:44 +0530

    obj-dec-expense

--- PROJECT/expense-tracker/src/components/ExpenseItem.jsx
+++ PROJECT/expense-tracker/src/components/ExpenseItem.jsx
@@ -3,15 +3,15 @@ import './ExpenseItem.css'
 
 const ExpenseItem=(props)=>{
   console.log(props)
-  const date=new Date().toISOString();
-  const amount=10;
-  const title=""
+  let {title=" ",
+  amount=0,
+  date=new Date()}=props
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h1>{props.title}</h1>
-        <div className="expense-item__price">${props.amount}</div>
+        <h1>{title}</h1>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   )
