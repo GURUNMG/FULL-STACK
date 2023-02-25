db9975ea3acb2c26d3c554f1bc4f67b75fcf76d0
GURUBHARAN N M <gurubharan.cs20@bitsathy.ac.in>
2023-02-25 10:11:54 +0530

    event

--- FRONTEND/dom.js
+++ FRONTEND/dom.js
@@ -52,12 +52,27 @@
 //getting what are the tags inside the element
 // console.log(body.children)
 
-const ul=document.getElementById('first-ul')
+// const ul=document.getElementById('first-ul')
 // getting the tags inisde the unordered list..
-  
-const inn=ul.children;
-let n=0;
-for (const itr of inn) {
-  itr.innerText=++n;
-  
+// const inn=ul.children;
+// let n=0;
+// for (const itr of inn) {
+//   itr.innerText=++n;
+//   itr.classList.add('lii')
+// }
+const changeBy=()=>{
+  const ul=document.getElementById('first-ul')
+  const inn=ul.children;
+  let n=0;  
+  for (const itr of inn) {
+    itr.innerText=++n;
+    itr.classList.toggle('lii')
+    // toggle is used to add the class name when it is not present
+    //if the class name is present it will remove it ..
+  }
 }
+
+const btn=document.getElementById('clr');
+btn.addEventListener('click',changeBy)
+// setInterval(changeBy,1000)
+
