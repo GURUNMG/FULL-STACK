77d77ae9798bad09ac77bb34d1c5d1403ad79c69
GURUBHARAN N M <gurubharan.cs20@bitsathy.ac.in>
2023-02-25 15:17:43 +0530

    expense details

--- PROJECT/expense-tracker/src/App.js
+++ PROJECT/expense-tracker/src/App.js
@@ -1,6 +1,27 @@
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 function App() {
+  const DUMMY_DATA = [
+    {
+      id: 'e1',
+      title: 'Groceries',
+      amount: 94.12,
+      date: new Date(2021, 7, 14),
+    },
+    { id: 'e2', title: 'New Mobile', amount: 799.49, date: new Date(2020, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Repair',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New work desk',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
   return (
     <div>
     <ExpenseItem 
